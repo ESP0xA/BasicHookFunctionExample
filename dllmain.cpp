1d9e226c78257f3be8f3d4edce24a26a39cf5790
zhangxingyu <rpm@live.com>
2020-12-14 17:52:36 +0800

    add some comments

--- dllmain.cpp
+++ dllmain.cpp
@@ -34,13 +34,14 @@ void __declspec(naked) ourFunct() {
 
 DWORD WINAPI MainThread(LPVOID param) {
 	/* the assembly lines where we are going to hook is looks like this :
-		2B 4D 08      subecx,[ebp+08]
-		8B 55 F8      movedx,[ebp-08]
-		89 0A         mov[edx],ecx
+		2B 4D 08      subecx, [ebp + 08]
+		8B 55 F8      movedx, [ebp - 08]
+		89 0A         mov[edx], ecx
 	*/
 	int hookLength = 6;	//There are 3 bytes(< 5) in the first line, the hook funtion contains at least 1 `jmp` instrution(5 bytes) 
-						// so that the offset length should be at least 5. But if we are using 5, we can only over
-	DWORD hookAddress = 0x8d2768;
+						// so that the offset length should be at least 5. But if we are using 5, we can only override the first line
+						// and part of second line.
+	DWORD hookAddress = 0x8d2768;	// signature if this address: 2B 4D 08 8B 55 F8
 	jmpBackAddy = hookAddress + hookLength;
 
 	Hook((void*)hookAddress, ourFunct, hookLength);
